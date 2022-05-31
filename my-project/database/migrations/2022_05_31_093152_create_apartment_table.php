7fc976155b295c2cca65737203f7684c0266ebde
luismi <luismipedriza@gmail.com>
2022-05-31 12:00:07 +0200

    tabla apartamentos

---
+++ my-project/database/migrations/2022_05_31_093152_create_apartment_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartment', function (Blueprint $table) {
+            $table->id();
+            $table->text('direccion');
+            $table->integer('codigo postal');
+            $table->enum('habitaciones', ['1', '2','3','4','5','6']);
+            $table->enum('camas_dobles', ['1', '2','3','4','5','6']);
+            $table->enum('camas_indiv', ['1', '2','3','4','5','6','7','8']);
+            $table->enum('baños', ['1', '2','3','4','5','6']);
+            $table->enum('solarium', ['si', 'no']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartment');
+    }
+};
