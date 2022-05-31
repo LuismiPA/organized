db534ed95d7ffad800494a98eab0f2ab5cef2d1d
luismi <luismipedriza@gmail.com>
2022-05-31 11:59:53 +0200

    tabla apartamentos

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -24,4 +24,5 @@ Route::get('/', [App\Http\Controllers\PagesController::class, 'login'])->name('a
 
 Auth::routes();
 
+
 /* Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home'); */
