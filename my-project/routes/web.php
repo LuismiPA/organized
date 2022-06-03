1eb568309fffdb157f08c3c0a14857c076c69376
Luismi <luismipedriza@gmail.com>
2022-06-03 15:59:37 +0200

    propietario middle

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -19,16 +19,18 @@ Route::get('/', [App\Http\Controllers\PagesController::class, 'index'])->name('i
 
 
 Route::get('/', [App\Http\Controllers\PagesController::class, 'login'])->name('auth.login');
-Route::get('/user/panel', [App\Http\Controllers\PagesController::class, 'userPage'])->name('user.panel');
 
 Route::middleware('auth', 'admin')->group(function () {
     Route::get('/admin/panel', [App\Http\Controllers\PagesController::class, 'adminPage'])->middleware('admin')->name('admin.panel');
     Route::get('/admin/crear', [App\Http\Controllers\UserController::class, 'crear'])->name('admin.crear');
-        Route::post('/admin/crear', [App\Http\Controllers\UserController::class, 'crear_usuario'])->name('admin.crear_usuario');
+    Route::post('/admin/crear', [App\Http\Controllers\UserController::class, 'crear_usuario'])->name('admin.crear_usuario');
     Route::get('/admin/editar/{id}', [App\Http\Controllers\UserController::class, 'editar'])->name('admin.editar');
-        Route::put('/admin/editar/{id}', [App\Http\Controllers\UserController::class, 'editado'])->name('admin.editado');
+    Route::put('/admin/editar/{id}', [App\Http\Controllers\UserController::class, 'editado'])->name('admin.editado');
 });
 
+Route::middleware('auth', 'propietario')->group(function () {
+    Route::get('/user/panel', [App\Http\Controllers\PagesController::class, 'userPage'])->name('user.panel');
+});
 
 
 
